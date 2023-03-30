3abf6adcfe8b04361beb724b57d14725d122db50
iburley33 <iburley33@gmail.com>
2023-03-30 19:31:24 -0400

    ready for testing

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -21,7 +21,7 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
-  Category.findOne(
+  Product.findOne(
     {
       where: { 
         id: req.params.id 
